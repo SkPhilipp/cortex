01c0f1430dedac1d7342e92a3a92481b557321ba
Philipp Gayret <philipp@gayret.nl>
2018-09-23 16:42:50 +0200

    Updated pathing and demos

--- src/test/java/com/hileco/cortex/demo/PathingDemo.java
+++ src/test/java/com/hileco/cortex/demo/PathingDemo.java
@@ -9,13 +9,15 @@ import com.hileco.cortex.analysis.processors.KnownLoadProcessor;
 import com.hileco.cortex.analysis.processors.KnownProcessor;
 import com.hileco.cortex.analysis.processors.ParameterProcessor;
 import com.hileco.cortex.fuzzer.ProgramGenerator;
+import com.hileco.cortex.instructions.jumps.JUMP_DESTINATION;
 import com.hileco.cortex.mapping.TreeMapper;
+import com.hileco.cortex.pathing.PathIterator;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class TreeMapperDemo {
+public class PathingDemo {
 
     private static final long SEED = 0L;
 
@@ -37,9 +39,27 @@ public class TreeMapperDemo {
         var treeMapper = new TreeMapper();
         var treeMapping = treeMapper.map(tree);
         System.out.println(program);
-        treeMapping.getJumpMapping().forEach((source, targets) -> {
-            System.out.println(String.format("%04d", source));
-            targets.forEach(target -> System.out.println(String.format(" --> %04d", target)));
+        var pathIterator = new PathIterator(treeMapping, 1);
+        var instructions = tree.toInstructions();
+        pathIterator.forEachRemaining(integers -> {
+            System.out.println("        ┌───────────────────────────────────");
+            for (var i = 0; i < integers.size() - 1; i++) {
+                var current = integers.get(i);
+                var next = integers.get(i + 1);
+                var instruction = instructions.get(current);
+                if (current == 1 || instruction instanceof JUMP_DESTINATION) {
+                    System.out.println(String.format("        │ %% %d through %d", current, next));
+                    for (int index = current; index <= next; index++) {
+                        instruction = instructions.get(index);
+                        System.out.println(String.format(" %06d │ %s", index, instruction));
+                    }
+                }
+            }
+            var current = integers.get(integers.size() - 1);
+            var instruction = instructions.get(current);
+            System.out.println("        │ % ending block");
+            System.out.println(String.format(" %06d │ %s", current, instruction));
+            System.out.println("        └───────────────────────────────────");
         });
     }
 }
