2f8df015520e655abdea2cfd3cc3b241f3f06337
Philipp Gayret <philipp@gayret.nl>
2018-09-28 17:50:32 +0200

    Cosmetic cleanups

--- src/main/java/com/hileco/cortex/server/CortexServer.java
+++ src/main/java/com/hileco/cortex/server/CortexServer.java
@@ -9,14 +9,11 @@ import com.hileco.cortex.server.api.demo.DemoOptimizerApi;
 import com.hileco.cortex.server.api.demo.DemoPathingApi;
 import com.hileco.cortex.server.api.docs.DocsInstructionsListApi;
 import spark.ResponseTransformer;
-
-import static spark.Spark.after;
-import static spark.Spark.get;
-import static spark.Spark.path;
-import static spark.Spark.staticFileLocation;
+import spark.Spark;
 
 public class CortexServer {
 
+    private static final int SERVER_PORT = 8080;
     private static final ObjectMapper OBJECT_MAPPER;
     private static final ResponseTransformer JSON;
 
@@ -28,19 +25,20 @@ public class CortexServer {
     }
 
     public static void main(String[] args) {
-        staticFileLocation("/public");
-        path("/api", () -> {
-            path("/demo", () -> {
-                get("/constraints.json", new DemoConstraintApi(), JSON);
-                get("/fuzzer.json", new DemoFuzzerApi(), JSON);
-                get("/pathing.json", new DemoPathingApi(), JSON);
-                get("/program.json", new DemoOptimizerApi(), JSON);
-                get("/jump-mapping.json", new DemoJumpMappingApi(), JSON);
+        Spark.port(SERVER_PORT);
+        Spark.staticFileLocation("/public");
+        Spark.path("/api", () -> {
+            Spark.path("/demo", () -> {
+                Spark.get("/constraints.json", new DemoConstraintApi(), JSON);
+                Spark.get("/fuzzer.json", new DemoFuzzerApi(), JSON);
+                Spark.get("/pathing.json", new DemoPathingApi(), JSON);
+                Spark.get("/program.json", new DemoOptimizerApi(), JSON);
+                Spark.get("/jump-mapping.json", new DemoJumpMappingApi(), JSON);
             });
-            path("/docs", () -> {
-                get("/instructions.json", new DocsInstructionsListApi(), JSON);
+            Spark.path("/docs", () -> {
+                Spark.get("/instructions.json", new DocsInstructionsListApi(), JSON);
             });
         });
-        after((request, response) -> response.type("application/json"));
+        Spark.after((request, response) -> response.type("application/json"));
     }
 }
