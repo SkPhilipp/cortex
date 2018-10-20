b3f53ce5ffc709da27d93f12ac5372373ae00d8b
Philipp Gayret <philipp@gayret.nl>
2018-10-20 13:15:48 +0200

    Updated expression builer to use sliding map

--- src/main/java/com/hileco/cortex/constraints/ExpressionBuilder.java
+++ src/main/java/com/hileco/cortex/constraints/ExpressionBuilder.java
@@ -10,80 +10,140 @@ import com.hileco.cortex.instructions.math.ADD;
 import com.hileco.cortex.instructions.math.MODULO;
 import com.hileco.cortex.instructions.math.MULTIPLY;
 import com.hileco.cortex.instructions.math.SUBTRACT;
+import com.hileco.cortex.instructions.stack.POP;
 import com.hileco.cortex.instructions.stack.PUSH;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class ExpressionBuilder {
 
-    private Map<Integer, Consumer<Expression>> consumers;
-    private int offset;
+    private final SlidingConsumerMap<Expression> map = new SlidingConsumerMap<>();
 
-    public ExpressionBuilder() {
-        this.consumers = new HashMap<>();
-    }
-
-    public void bind(int position, Consumer<Expression> consumer) {
-        var existingConsumer = this.consumers.get(this.offset + position);
-        var newConsumer = consumer;
-        if (existingConsumer != null) {
-            newConsumer = newConsumer.andThen(existingConsumer);
-        }
-        this.consumers.put(this.offset + position, newConsumer);
-    }
-
-    private void release(int position, Expression expression) {
-        var consumer = this.consumers.remove(this.offset + position);
-        if (consumer != null) {
-            consumer.accept(expression);
-        }
+    public void bindConstraint(int position, Consumer<Expression> value) {
+        this.map.set(position, value);
     }
 
     public void addInstruction(Instruction instruction) {
-        Expression expression = null;
-        var stackAdds = instruction.getStackAdds();
-        this.offset += stackAdds.size();
 
+        Expression expression = null;
+        var consumersToFill = instruction.getStackAdds()
+                .stream()
+                .map(this.map::remove)
+                .collect(Collectors.toList());
+
+        instruction.getStackAdds().forEach(entry -> this.map.backward());
+
+        // BITWISE_NOT
+
+        // BITWISE_AND
+
+        // BITWISE_OR
+
+        // BITWISE_XOR
+
+        // CALL
+        // --
+
+        // CALL_RETURN
+        // --
+
+        // EQUALS
+
+        // GREATER_THAN
+
+        // IS_ZERO
+
+        // LESS_THAN
+
+        // HALT
+        // --
+
+        // NOOP
+        // --
+
+        // LOAD
+        if (instruction instanceof LOAD) {
+            var loadInstruction = (LOAD) instruction;
+            var referenceExpression = ReferenceExpression.reference(loadInstruction.getProgramStoreZone(), null);
+            this.map.set(LOAD.ADDRESS.getPosition(), referenceExpression::setAddress);
+            expression = referenceExpression;
+        }
+
+        // SAVE
+        // --
+
+        // EXIT
+        // --
+
+        // JUMP
+        // --
+
+        // JUMP_DESTINATION
+        // --
+
+        // JUMP_IF
+        // --
+
+        // ADD
         if (instruction instanceof ADD) {
             var operation2Expression = Operation2Expression.Type2.ADD.on(null, null);
-            this.bind(ADD.LEFT.getPosition(), operation2Expression::setLeft);
-            this.bind(ADD.RIGHT.getPosition(), operation2Expression::setRight);
+            this.map.set(ADD.LEFT.getPosition(), operation2Expression::setLeft);
+            this.map.set(ADD.RIGHT.getPosition(), operation2Expression::setRight);
             expression = operation2Expression;
         }
-        if (instruction instanceof SUBTRACT) {
-            var operation2Expression = Operation2Expression.Type2.SUBTRACT.on(null, null);
-            this.bind(SUBTRACT.LEFT.getPosition(), operation2Expression::setLeft);
-            this.bind(SUBTRACT.RIGHT.getPosition(), operation2Expression::setRight);
-            expression = operation2Expression;
-        }
-        if (instruction instanceof MULTIPLY) {
-            var operation2Expression = Operation2Expression.Type2.MULTIPLY.on(null, null);
-            this.bind(MULTIPLY.LEFT.getPosition(), operation2Expression::setLeft);
-            this.bind(MULTIPLY.RIGHT.getPosition(), operation2Expression::setRight);
-            expression = operation2Expression;
-        }
+
+        // DIVIDE
+
+        // HASH
+
+        // MODULO
         if (instruction instanceof MODULO) {
             var operation2Expression = Operation2Expression.Type2.MODULO.on(null, null);
-            this.bind(MODULO.LEFT.getPosition(), operation2Expression::setLeft);
-            this.bind(MODULO.RIGHT.getPosition(), operation2Expression::setRight);
+            this.map.set(MODULO.LEFT.getPosition(), operation2Expression::setLeft);
+            this.map.set(MODULO.RIGHT.getPosition(), operation2Expression::setRight);
             expression = operation2Expression;
         }
+
+        // SUBTRACT
+        if (instruction instanceof SUBTRACT) {
+            var operation2Expression = Operation2Expression.Type2.SUBTRACT.on(null, null);
+            this.map.set(SUBTRACT.LEFT.getPosition(), operation2Expression::setLeft);
+            this.map.set(SUBTRACT.RIGHT.getPosition(), operation2Expression::setRight);
+            expression = operation2Expression;
+        }
+
+        // MULTIPLY
+        if (instruction instanceof MULTIPLY) {
+            var operation2Expression = Operation2Expression.Type2.MULTIPLY.on(null, null);
+            this.map.set(MULTIPLY.LEFT.getPosition(), operation2Expression::setLeft);
+            this.map.set(MULTIPLY.RIGHT.getPosition(), operation2Expression::setRight);
+            expression = operation2Expression;
+        }
+
+        // DUPLICATE
+
+        // POP
+        if (instruction instanceof POP) {
+            this.map.set(POP.INPUT.getPosition(), (ignored) -> {
+            });
+        }
+
+        // PUSH
         if (instruction instanceof PUSH) {
             var pushInstruction = (PUSH) instruction;
             var value = new BigInteger(pushInstruction.getBytes());
             expression = ValueExpression.value(value.longValue());
         }
-        if (instruction instanceof LOAD) {
-            var loadInstruction = (LOAD) instruction;
-            var referenceExpression = ReferenceExpression.reference(loadInstruction.getProgramStoreZone(), null);
-            this.bind(LOAD.ADDRESS.getPosition(), referenceExpression::setAddress);
-            expression = referenceExpression;
-        }
-        for (var stackAdd : stackAdds) {
-            this.release(stackAdd, expression);
+
+        // SWAP
+
+        var stackParameters = instruction.getStackParameters();
+        stackParameters.forEach(entry -> this.map.forward());
+
+        for (var consumer : consumersToFill) {
+            consumer.accept(expression);
         }
     }
 }
--- src/test/java/com/hileco/cortex/constraints/ExpressionBuilderTest.java
+++ src/test/java/com/hileco/cortex/constraints/ExpressionBuilderTest.java
@@ -5,6 +5,7 @@ import com.hileco.cortex.instructions.debug.NOOP;
 import com.hileco.cortex.instructions.io.LOAD;
 import com.hileco.cortex.instructions.jumps.JUMP_IF;
 import com.hileco.cortex.instructions.math.SUBTRACT;
+import com.hileco.cortex.instructions.stack.POP;
 import com.hileco.cortex.instructions.stack.PUSH;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,48 +19,50 @@ import static com.hileco.cortex.constraints.expressions.ValueExpression.value;
 public class ExpressionBuilderTest {
 
     @Test
-    public void testConstraintBinding() {
+    public void testParameterized() {
         var expressionBuilder = new ExpressionBuilder();
         var constraint = EQUAL_TO.on(null, value(0L));
-        expressionBuilder.bind(0, constraint::setLeft);
+        expressionBuilder.bindConstraint(-1, constraint::setLeft);
         expressionBuilder.addInstruction(new SUBTRACT());
+        expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(1L).toByteArray()));
         expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
-        expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
-        var solver = new Solver();
-        var solution = solver.solve(constraint);
-        Assert.assertTrue(solution.isSolvable());
         Assert.assertEquals("((123 - 123) == 0)", constraint.toString());
     }
 
     @Test
-    public void testConstraintMidProcess() {
+    public void testPop() {
+        var expressionBuilder = new ExpressionBuilder();
+        var constraint = EQUAL_TO.on(null, value(0L));
+        expressionBuilder.bindConstraint(-1, constraint::setLeft);
+        expressionBuilder.addInstruction(new SUBTRACT());
+        expressionBuilder.addInstruction(new POP());
+        expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(1L).toByteArray()));
+        expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
+        expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
+        Assert.assertEquals("((123 - 123) == 0)", constraint.toString());
+    }
+
+    @Test
+    public void testConstraintBetweenInstructions() {
         var expressionBuilder = new ExpressionBuilder();
         var constraint = EQUAL_TO.on(null, value(0L));
         expressionBuilder.addInstruction(new JUMP_IF());
         expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(246L).toByteArray()));
-        expressionBuilder.bind(0, constraint::setLeft);
+        expressionBuilder.bindConstraint(-1, constraint::setLeft);
         expressionBuilder.addInstruction(new SUBTRACT());
         expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
         expressionBuilder.addInstruction(new NOOP());
         expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(123L).toByteArray()));
-        var solver = new Solver();
-        var solution = solver.solve(constraint);
-        Assert.assertTrue(solution.isSolvable());
         Assert.assertEquals("((123 - 123) == 0)", constraint.toString());
     }
 
     @Test
-    public void testConstraintWithParameter() {
+    public void testReferences() {
         var expressionBuilder = new ExpressionBuilder();
         var constraint = EQUAL_TO.on(null, value(0L));
-        expressionBuilder.bind(0, constraint::setLeft);
+        expressionBuilder.bindConstraint(-1, constraint::setLeft);
         expressionBuilder.addInstruction(new LOAD(ProgramStoreZone.CALL_DATA));
         expressionBuilder.addInstruction(new PUSH(BigInteger.valueOf(10L).toByteArray()));
-        var solver = new Solver();
-        var solution = solver.solve(constraint);
-        Assert.assertTrue(solution.isSolvable());
-        Assert.assertEquals(1, solution.getPossibleValues().size());
-        Assert.assertEquals(0, solution.getPossibleValues().get(constraint.getLeft()).intValue());
         Assert.assertEquals("(CALL_DATA[10] == 0)", constraint.toString());
     }
 }
