f168153c9546bc5bb1beece175c2088462e0513a
Philipp Gayret <philipp@gayret.nl>
2019-01-18 01:57:17 +0100

    Updated deploy script to deploy html only

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -6,7 +6,3 @@
 # Deploy Documentation
 ssh root@cortex "rm -rf /var/www/html/*"
 scp -r ./build/asciidoc/html5/. root@cortex:/var/www/html
-
-# Deploy Application
-ssh cortex@cortex "pkill -f java"
-scp ./build/libs/cortex.jar cortex@cortex:cortex.jar
