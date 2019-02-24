19f6d35c083fd861d7f7b4b03ea6b863d6929900
Philipp Gayret <philipp@gayret.nl>
2019-02-24 15:28:31 +0100

    Added tests to ExpressionOptimizer, and rudimentary implementation of it for SymbolicProgramExplorer

--- src/main/java/com/hileco/cortex/constraints/expressions/ExpressionOptimizer.kt
+++ src/main/java/com/hileco/cortex/constraints/expressions/ExpressionOptimizer.kt
@@ -1,104 +1,107 @@
 package com.hileco.cortex.constraints.expressions
 
+import com.hileco.cortex.constraints.expressions.Expression.*
 import com.hileco.cortex.instructions.math.*
 
 class ExpressionOptimizer {
     fun optimize(expression: Expression): Expression {
         when (expression) {
-            is Expression.Add -> {
+            is Add -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
                     val result = ADD_INSTRUCTION.calculate(expression.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                    return Expression.Value(result.toLong())
-                } else if (expression.left is Expression.Add && expression.right is Expression.Value) {
+                    return Value(result.toLong())
+                } else if (expression.left is Add && expression.right is Expression.Value) {
                     if (expression.left.left is Expression.Value) {
                         val result = ADD_INSTRUCTION.calculate(expression.left.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                        return Expression.Add(expression.left.right, Expression.Value(result.toLong()))
+                        return Add(expression.left.right, Value(result.toLong()))
                     } else if (expression.left.right is Expression.Value) {
                         val result = ADD_INSTRUCTION.calculate(expression.left.right.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                        return Expression.Add(expression.left.left, Expression.Value(result.toLong()))
+                        return Add(expression.left.left, Value(result.toLong()))
                     }
-                } else if (expression.right is Expression.Add && expression.left is Expression.Value) {
+                } else if (expression.right is Add && expression.left is Expression.Value) {
                     if (expression.right.left is Expression.Value) {
                         val result = ADD_INSTRUCTION.calculate(expression.right.left.constant.toBigInteger(), expression.left.constant.toBigInteger())
-                        return Expression.Add(expression.right.right, Expression.Value(result.toLong()))
+                        return Add(expression.right.right, Value(result.toLong()))
                     } else if (expression.right.right is Expression.Value) {
                         val result = ADD_INSTRUCTION.calculate(expression.right.right.constant.toBigInteger(), expression.left.constant.toBigInteger())
-                        return Expression.Add(expression.right.left, Expression.Value(result.toLong()))
+                        return Add(expression.right.left, Value(result.toLong()))
                     }
                 }
             }
-            is Expression.And -> {
-                val falseInputs = expression.inputs.count(IS_EQUIVALENT_FALSE)
-                val trueInputs = expression.inputs.count(IS_EQUIVALENT_TRUE)
+            is And -> {
+                val distinctInputs = expression.inputs.distinct()
+                val falseInputs = distinctInputs.count(IS_EQUIVALENT_FALSE)
+                val trueInputs = distinctInputs.count(IS_EQUIVALENT_TRUE)
                 when {
-                    falseInputs > 0 -> return Expression.False
-                    trueInputs == expression.inputs.size -> return Expression.True
-                    trueInputs > 0 -> return Expression.And(expression.inputs.filterNot(IS_EQUIVALENT_TRUE))
+                    falseInputs > 0 -> return False
+                    trueInputs == distinctInputs.size -> return True
+                    trueInputs > 0 -> return And(distinctInputs.filterNot(IS_EQUIVALENT_TRUE))
+                    distinctInputs.size < expression.inputs.size -> return And(distinctInputs)
                 }
             }
-            is Expression.Divide -> {
+            is Divide -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
                     val result = DIDIVE_INSTRUCTION.calculate(expression.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                    return Expression.Value(result.toLong())
+                    return Value(result.toLong())
                 }
             }
-            is Expression.Equals -> {
+            is Equals -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
-                    return if (expression.left.constant == expression.right.constant) Expression.True else Expression.False
+                    return if (expression.left.constant == expression.right.constant) True else False
                 }
             }
-            is Expression.GreaterThan -> {
+            is GreaterThan -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
-                    return if (expression.left.constant > expression.right.constant) Expression.True else Expression.False
+                    return if (expression.left.constant > expression.right.constant) True else False
                 }
             }
-            is Expression.IsZero -> {
+            is IsZero -> {
                 if (expression.input is Expression.Value) {
-                    return if (expression.input.constant == 0L) Expression.True else Expression.False
+                    return if (expression.input.constant == 0L) True else False
                 }
             }
-            is Expression.LessThan -> {
+            is LessThan -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
-                    return if (expression.left.constant < expression.right.constant) Expression.True else Expression.False
+                    return if (expression.left.constant < expression.right.constant) True else False
                 }
             }
-            is Expression.Modulo -> {
+            is Modulo -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
                     val result = MODULO_INSTRUCTION.calculate(expression.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                    return Expression.Value(result.toLong())
+                    return Value(result.toLong())
                 }
             }
-            is Expression.Multiply -> {
+            is Multiply -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
                     val result = MULTIPLY_INSTRUCTION.calculate(expression.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                    return Expression.Value(result.toLong())
+                    return Value(result.toLong())
                 }
             }
-            is Expression.Not -> {
+            is Not -> {
                 when (expression.input) {
-                    Expression.True -> return Expression.False
-                    Expression.False -> return Expression.True
+                    True -> return False
+                    False -> return True
                 }
             }
-            is Expression.Subtract -> {
+            is Subtract -> {
                 if (expression.left is Expression.Value && expression.right is Expression.Value) {
                     val result = SUBTRACT_INSTRUCTION.calculate(expression.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                    return Expression.Value(result.toLong())
-                } else if (expression.left is Expression.Add && expression.right is Expression.Value) {
+                    return Value(result.toLong())
+                } else if (expression.left is Subtract && expression.right is Expression.Value) {
                     if (expression.left.left is Expression.Value) {
                         val result = SUBTRACT_INSTRUCTION.calculate(expression.left.left.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                        return Expression.Add(expression.left.right, Expression.Value(result.toLong()))
+                        return Subtract(expression.left.right, Value(result.toLong()))
                     } else if (expression.left.right is Expression.Value) {
                         val result = SUBTRACT_INSTRUCTION.calculate(expression.left.right.constant.toBigInteger(), expression.right.constant.toBigInteger())
-                        return Expression.Add(expression.left.left, Expression.Value(result.toLong()))
+                        return Subtract(expression.left.left, Value(result.toLong()))
                     }
-                } else if (expression.right is Expression.Add && expression.left is Expression.Value) {
+                } else if (expression.right is Subtract && expression.left is Expression.Value) {
                     if (expression.right.left is Expression.Value) {
                         val result = SUBTRACT_INSTRUCTION.calculate(expression.right.left.constant.toBigInteger(), expression.left.constant.toBigInteger())
-                        return Expression.Add(expression.right.right, Expression.Value(result.toLong()))
+                        return Subtract(expression.right.right, Value(result.toLong()))
                     } else if (expression.right.right is Expression.Value) {
                         val result = SUBTRACT_INSTRUCTION.calculate(expression.right.right.constant.toBigInteger(), expression.left.constant.toBigInteger())
-                        return Expression.Add(expression.right.left, Expression.Value(result.toLong()))
+                        return Subtract(expression.right.left, Value(result.toLong()))
                     }
                 }
             }
@@ -107,8 +110,8 @@ class ExpressionOptimizer {
     }
 
     companion object {
-        val IS_EQUIVALENT_TRUE: (Expression) -> Boolean = { it == Expression.True || (it is Expression.Value && it.constant >= 0) }
-        val IS_EQUIVALENT_FALSE: (Expression) -> Boolean = { it == Expression.False || it == Expression.Value(0) }
+        val IS_EQUIVALENT_TRUE: (Expression) -> Boolean = { it == True || (it is Expression.Value && it.constant >= 0) }
+        val IS_EQUIVALENT_FALSE: (Expression) -> Boolean = { it == False || it == Value(0) }
         val MULTIPLY_INSTRUCTION = MULTIPLY()
         val MODULO_INSTRUCTION = MODULO()
         val DIDIVE_INSTRUCTION = DIVIDE()
