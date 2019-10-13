1f892429542e0ea42e8ad8e9a41eaa34c21610a9
Philipp Gayret <philipp@hileco.com>
2019-10-13 15:23:47 +0200

    Updated documentation

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 # Build Documentation
-./gradlew clean asciidoctor
+gradle asciidoctor
 
 # Deploy Documentation
 ssh root@cortex-production "rm -rf /var/www/html/*"
-scp -r ./build/asciidoc/html5/index.html root@cortex-production:/var/www/html
+scp -r cortex-vm/build/asciidoc/html5/* root@cortex-production:/var/www/html
+scp -r cortex-fuzzing/build/asciidoc/html5/* root@cortex-production:/var/www/html
+scp -r cortex-symbolic/build/asciidoc/html5/* root@cortex-production:/var/www/html
+scp -r cortex-analysis/build/asciidoc/html5/* root@cortex-production:/var/www/html
+scp -r cortex-ethereum/build/asciidoc/html5/* root@cortex-production:/var/www/html
+scp -r cortex-documentation/build/asciidoc/html5/* root@cortex-production:/var/www/html
