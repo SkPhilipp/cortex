8c18c909a661dd352676dda86bf6876cbdb47f6e
Philipp Gayret <philipp.gayret@persgroep.net>
2020-07-12 00:32:17 +0200

    Light cleanup to cortex sandbox

--- cortex-sandbox/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-ls geth-installer.exe || curl https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.9.15-0f77f34b.exe --output geth-installer.exe
-mkdir -p datadir
-echo to install Geth run: ./geth-installer.exe
-echo run installed Geth using: geth --datadir datadir init genesis.json
