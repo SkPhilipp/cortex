69af97560ba01b316b80a6ed2de555855c9fce4e
Philipp Gayret <philipp.gayret@persgroep.net>
2020-07-18 23:01:34 +0200

    Path stream now functions with instruction width

--- cortex-analysis/src/main/kotlin/com/hileco/cortex/analysis/attack/PathStream.kt
+++ cortex-analysis/src/main/kotlin/com/hileco/cortex/analysis/attack/PathStream.kt
@@ -6,15 +6,29 @@ import com.hileco.cortex.vm.instructions.Instruction
 
 class PathStream(private val instructions: List<Instruction>,
                  private val path: List<Flow>) {
+
+    val instructionsAbsolute: Map<Int, Instruction>
+
+    init {
+        var position = 0
+        instructionsAbsolute = instructions.map { instruction ->
+            val result = position to instruction
+            position += instruction.width
+            result
+        }.toMap()
+    }
+
     fun asSequence() = sequence {
         path.forEachIndexed { index, flow ->
             if (flow.type == FlowType.PROGRAM_FLOW) {
                 val next = if (index + 1 < path.size) path[index + 1] else null
                 val source = flow.source
                 val target = next?.source ?: flow.target!!
-                for (line in source..target) {
-                    val instruction = instructions[line]
-                    yield(PathStreamElement(instruction, line, flow, next))
+                var position = source
+                while (position <= target) {
+                    val instruction = instructionsAbsolute[position] ?: throw IndexOutOfBoundsException("Index $position is not a mapped instruction")
+                    yield(PathStreamElement(instruction, position, flow, next))
+                    position += instruction.width
                 }
             }
         }
