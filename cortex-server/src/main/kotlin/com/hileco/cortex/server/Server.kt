3e788c8d3e6a9b0622aff1a593204e44f0e23250
Philipp Gayret <philipp.gayret@persgroep.net>
2020-08-06 19:38:08 +0200

    Added explorer upload and view samples

--- cortex-server/src/main/kotlin/com/hileco/cortex/server/Server.kt
+++ cortex-server/src/main/kotlin/com/hileco/cortex/server/Server.kt
@@ -6,15 +6,21 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.http.content.PartData
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
 import io.ktor.pebble.Pebble
 import io.ktor.pebble.PebbleContent
+import io.ktor.request.receiveMultipart
 import io.ktor.response.respond
+import io.ktor.response.respondRedirect
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+
+val bytecodes = ArrayList<String>()
 
 fun Application.module() {
     install(DefaultHeaders)
@@ -30,6 +36,29 @@ fun Application.module() {
         }
         get("/") {
             call.respond(PebbleContent("index.html", mapOf()))
+        }
+        get("/explorer/upload") {
+            call.respond(PebbleContent("explorer-upload.html", mapOf()))
+        }
+        post("/explorer/upload") {
+            val multipart = call.receiveMultipart()
+            var bytecode = ""
+            while (true) {
+                val part = multipart.readPart() ?: break
+                if (part is PartData.FormItem && part.name == "bytecode") {
+                    bytecode = part.value
+                }
+                part.dispose()
+            }
+            bytecodes.add(bytecode)
+            call.respondRedirect("/explorer/view/" + (bytecodes.size - 1))
+        }
+        get("/explorer/view/{id}") {
+            val id = call.parameters["id"] ?: throw IllegalArgumentException("Id missing")
+            call.respond(PebbleContent("explorer-view.html", mapOf(
+                    "id" to id,
+                    "bytecode" to bytecodes[id.toInt()]
+            )))
         }
     }
 }
