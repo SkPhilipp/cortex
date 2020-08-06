f73813ca8d237a5a59de63f7c05655bfa9436152
Philipp Gayret <philipp.gayret@persgroep.net>
2020-08-06 12:50:25 +0200

    Merged cortex-fe style and using Pebble for html templating

--- cortex-server/src/main/kotlin/com/hileco/cortex/server/Server.kt
+++ cortex-server/src/main/kotlin/com/hileco/cortex/server/Server.kt
@@ -1,12 +1,16 @@
 package com.hileco.cortex.server
 
+import com.mitchellbosecke.pebble.loader.ClasspathLoader
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.pebble.Pebble
+import io.ktor.pebble.PebbleContent
+import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.server.engine.embeddedServer
@@ -15,9 +19,17 @@ import io.ktor.server.netty.Netty
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(Pebble) {
+        loader(ClasspathLoader().apply {
+            prefix = "templates"
+        })
+    }
     install(Routing) {
+        static("css") {
+            resources("css")
+        }
         get("/") {
-            call.respondText("Cortex Online!", ContentType.Text.Html)
+            call.respond(PebbleContent("index.html", mapOf()))
         }
     }
 }
