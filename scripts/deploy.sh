c866cb137f5e61ce9cc7be8e3b421aa3f41e6bb6
Philipp Gayret <philipp.gayret@persgroep.net>
2020-09-08 01:51:20 +0200

    Added SSH and SCP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env bash
-
-# Build Documentation
-./gradlew clean build asciidoctor
+#!/usr/bin/env sh
 
 # Deploy Documentation
-ssh root@cortex-production "rm -rf /var/www/html/*"
-scp -r cortex-vm/build/asciidoc/html5/* root@cortex-production:/var/www/html
-scp -r cortex-fuzzing/build/asciidoc/html5/* root@cortex-production:/var/www/html
-scp -r cortex-symbolic/build/asciidoc/html5/* root@cortex-production:/var/www/html
-scp -r cortex-analysis/build/asciidoc/html5/* root@cortex-production:/var/www/html
-scp -r cortex-ethereum/build/asciidoc/html5/* root@cortex-production:/var/www/html
-scp -r cortex-documentation/build/asciidoc/html5/* root@cortex-production:/var/www/html
+ssh root@$CORTEX_DOCUMENTATION "rm -rf /var/www/html/*"
+scp -r cortex-vm/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
+scp -r cortex-fuzzing/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
+scp -r cortex-symbolic/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
+scp -r cortex-analysis/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
+scp -r cortex-ethereum/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
+scp -r cortex-documentation/build/asciidoc/html5/* root@$CORTEX_DOCUMENTATION:/var/www/html
